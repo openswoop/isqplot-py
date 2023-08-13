08bba32dd3b55a35fb75ab5b2d5a018491c3fd55
Andreas Ink <andreasink17@gmail.com>
2023-08-13 00:30:12 -0400

    add title, instructions, and rm csv text from selection

--- main.py
+++ main.py
@@ -2,15 +2,18 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 import os
+
+st.title("UNF Professor Rankings")
+st.subheader("Select a course code below")
 def file_selector(folder_path="./data/", type=""):
         folder_path = folder_path + type
         filenames = os.listdir(folder_path)
         csvFiles = []
         for file in filenames:
             if "csv" in file:
-                csvFiles.append(file)
+                csvFiles.append(file.replace(".csv", ""))
         selected_filename = st.selectbox("Select " + type, csvFiles)
-        return os.path.join(folder_path, selected_filename)
+        return os.path.join(folder_path, selected_filename + ".csv")
 selectedFile = file_selector()
 df = pd.read_csv(selectedFile)
 simpleDF = df[["instructor", "average_gpa", "rating"]]
